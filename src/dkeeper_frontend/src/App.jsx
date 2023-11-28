49ce07cf240eb3e63447627b8c60d1759fa95936
Vedant-asati <vedant.dev.jsr@gmail.com>
2023-11-28 08:49:19 +0530

    jjsr

--- src/dkeeper_frontend/src/App.jsx
+++ src/dkeeper_frontend/src/App.jsx
@@ -1,16 +1,60 @@
+// import React from 'react';
+// import  Header  from "./components/Header";
+// function App() {
+  //   return (
+    //     <div className="App">
+    //       <Header></Header>
+    //        <Router>
+    //         <Routes>
+    //           <Route path="/" element={<Header/>}/>
+    //         </Routes>
+    //       </Router>
+    //     </div>
+    //   );
+    // }
+    
+    // export default App;
 import { BrowserRouter as Router, Routes,Route,} from "react-router-dom";
-import React from 'react';
 import ReactDOM from 'react-dom';
-import  Header  from "./components/Header";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
+
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-    <div className="App">
-      <Header></Header>
-       <Router>
-        <Routes>
-          <Route path="/" element={<Header/>}/>
-        </Routes>
-      </Router>
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
--- src/dkeeper_frontend/src/components/Header.jsx
+++ src/dkeeper_frontend/src/components/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
 
-const Header = () => {
+function Header() {
   return (
-    <div>HeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeader</div>
-  )
+    <header>
+      <h1>
+        <HighlightIcon />
+        Keeper
+      </h1>
+    </header>
+  );
 }
 
 export default Header;
