3d7c4fdf38c80cac4f78a6bb103f541e818fec4f
Vedant-asati <vedant.dev.jsr@gmail.com>
2023-11-28 08:05:49 +0530

    hello

--- src/dkeeper_frontend/src/App.jsx
+++ src/dkeeper_frontend/src/App.jsx
@@ -3,6 +3,7 @@ import  Header  from "./components/Header";
 function App() {
   return (
     <div className="App">
+      <Header></Header>
        <Router>
         <Routes>
           <Route path="/" element={<Header/>}/>
