223eeaade85733b8fa9d2529f021c0f242713e3e
Vedant-asati <vedant.dev.jsr@gmail.com>
2023-12-06 15:36:10 +0530

    JSR Finalled for submission

--- src/dkeeper_frontend/src/components/Footer.jsx
+++ src/dkeeper_frontend/src/components/Footer.jsx
@@ -25,9 +25,9 @@ function Footer() {
   return (
     <footer className="footer1">
       <p className="icon-class">
-        <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/vedant-asati-ab278a252'><FontAwesomeIcon icon={faLinkedin} className="fa-icon" color="#4d4d4e" /></a>
-        <a target='_blank' rel='noreferrer' href='https://github.com/vedant-asati'><FontAwesomeIcon icon={faGithub} className="fa-icon" color="#4d4d4e" /></a>
-        <a target='_blank' rel='noreferrer' href='https://twitter.com/vedant_asati_'><FontAwesomeIcon icon={faTwitter} className="fa-icon" color="#4d4d4e" /></a>
+        <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/'><FontAwesomeIcon icon={faLinkedin} className="fa-icon" color="#4d4d4e" /></a>
+        <a target='_blank' rel='noreferrer' href='https://github.com/Vedant-asati/icp-jsr-1/'><FontAwesomeIcon icon={faGithub} className="fa-icon" color="#4d4d4e" /></a>
+        <a target='_blank' rel='noreferrer' href='https://twitter.com/'><FontAwesomeIcon icon={faTwitter} className="fa-icon" color="#4d4d4e" /></a>
       </p>
       <p>
         Copyright ⓒ {year}<br />
